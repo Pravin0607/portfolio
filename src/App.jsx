d6de95700d611f33324d9f0342be7ca06b08f829
Pravin Adhav <pravinadhav4945@gmail.com>
2022-11-30 19:45:15 +0530

    removed unwanted files and modified some content of app.js and modified README.md

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-full bg-emerald-200 flex justify-center items-center">
+      <h1 className="text-2xl text-center font-mono font-bold">Hello Stranger!</h1>
+    </div>
+  );
+}
+
+export default App;
